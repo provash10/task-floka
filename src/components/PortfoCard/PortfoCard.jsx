9fc74e16de172798b296358e767b7639a7095018
Provash <provash20cb@gmail.com>
2026-04-06 00:31:39 +0600

    All portfo cont create

--- src/components/PortfoCard/PortfoCard.jsx
+++ src/components/PortfoCard/PortfoCard.jsx
@@ -27,7 +27,7 @@ const PortfoCard = () => {
                 </div>
 
                 <div className='relative'>
-                    <img  src="https://floka.casethemes.net/wp-content/uploads/2025/05/home3-accordion1-655x450.jpg" alt="" />
+                    <img src="https://floka.casethemes.net/wp-content/uploads/2025/05/home3-accordion1-655x450.jpg" alt="" />
                     <img className='absolute top-2 left-2 p-4' src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon6.svg" alt="" />
                     <div className='flex absolute left-4 top-100 text-white'>
                         <h2>BRANDING,</h2>
@@ -42,19 +42,74 @@ const PortfoCard = () => {
 
             </div>
 
-            <div className='flex justify-between gap-4 w-full'>
-    
-    <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
-        <h1>ALDAN BRANDING</h1>
-        <h1>2025</h1>
-    </div>
+            <div className='flex justify-between gap-4 w-full mb-2'>
 
-    <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
-        <h1>ALDAN BRANDING</h1>
-        <h1>2025</h1>
-    </div>
+                <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
+                    <h1>ALDAN BRANDING</h1>
+                    <h1>2025</h1>
+                </div>
 
-</div>
+                <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
+                    <h1>ALDAN BRANDING</h1>
+                    <h1>2025</h1>
+                </div>
+
+            </div>
+
+            {/* One Card */}
+            <div>
+                <div className='relative '>
+                    <img className='rounded-2xl' src="https://floka.casethemes.net/wp-content/uploads/2025/05/home1-bg-img3-1320x600.webp" alt="" />
+                    <img className='absolute left-4 top-4 text-white' src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon5.svg" alt="" />
+                    <h1 className='absolute left-4 top-135 text-white'>BRANDING, UX</h1>
+                </div>
+
+                <div className='m-2'>
+                    <div className='flex justify-between w-full px-4 py-4 rounded-2xl bg-white border border-gray-200'>
+                        <h1>ALDAN BRANDING</h1>
+                        <h1>2025</h1>
+                    </div>
+                </div>
+            </div>
+
+            {/* again 2 card */}
+
+            <div className='flex justify-between gap-4 rounded-2xl mb-2'>
+                <div className='relative'>
+                    <img className='rounded-2xl' src="	https://floka.casethemes.net/wp-content/uploads/2025/05/home1-bg-img2-655x450.webp" alt="" />
+                    <img className='absolute top-2 left-2 p-4' src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon7.svg" alt="" />
+                    <h1 className='absolute left-4 top-100 text-white'>BRANDING, UX</h1>
+                </div>
+
+                <div className='relative'>
+                    <img src="https://floka.casethemes.net/wp-content/uploads/2025/05/home1-bg-img1-655x450.webp" alt="" />
+                    <img className='absolute top-2 left-2 p-4' src="https://floka.casethemes.net/wp-content/uploads/2025/05/home-1-icon3.svg" alt="" />
+                    <div className='flex absolute left-4 top-100 text-white'>
+                        <h2>BRANDING,</h2>
+                        <h2>MODULE,</h2>
+                        <h2>PRODUCT,</h2>
+                        <h2>UX,</h2>
+                        <h2>WEBSITE</h2>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className='flex justify-between gap-4 w-full mb-2'>
+
+                <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
+                    <h1>ALDAN BRANDING</h1>
+                    <h1>2025</h1>
+                </div>
+
+                <div className='flex justify-between w-1/2 px-4 py-4 rounded-2xl bg-white border border-gray-200'>
+                    <h1>ALDAN BRANDING</h1>
+                    <h1>2025</h1>
+                </div>
+
+            </div>
         </div>
     );
 };
